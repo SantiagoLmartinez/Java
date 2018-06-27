4f520fdf4b5f654ac9024f336f12abc5c7c1b984
Usuario <Usuario@192.168.0.11>
2018-06-27 20:13:04 -0300

    fibonacci

---
+++ ProgramaSimple2/src/programasimple2/ProgramaSimple2.java
@@ -0,0 +1,30 @@
+package programasimple2;
+
+public class ProgramaSimple2 {
+
+    public static void main(String[] args) {
+//Ejercicio:
+        // Imprime numeros la escala Fibonacci hasta el 144
+           int num1 = 1, num2 = 0;
+        
+        System.out.println(num2);
+        System.out.println(num1);
+        
+        for(int i = 0; i <= 10; i++){
+            num1 = num1 + num2;
+            num2 = num1 - num2;
+            System.out.println(num1);
+                
+            
+            }
+           
+        }
+        
+        
+        
+        }
+        
+        
+        
+
+
